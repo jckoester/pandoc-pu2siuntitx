8ef926a9864e76d80ecbf8d76e5f9e6082452bcc
Jan Köster <jan@jckoester.de>
2024-02-22 22:50:14 +0200

    First commit
    First version of the filter, no test, no readme

---
+++ pu2siunitx.lua
@@ -0,0 +1,150 @@
+--- ToDo
+--- * eigenständige Einheiten \unit{} abbilden
+
+
+local combinedunits = { 
+  "fmol", "pmol", "nmol", "mmol", "mol", "kmol",
+  "fg", "pg", "ng", "mg", "g", "kg",
+  "pm", "nm", "mm", "cm", "dm", "km",
+  "as", "fs", "ps", "ns", "ms", "s",
+  "pA", "nA", "mA", "A", "kA",
+  "mL", "L", "hL", "ml", "hl", "l",
+  "mHz", "Hz", "kHz", "MHz", "GHz", "THz",
+  "mN", "N", "kN", "MN",
+  "Pa", "kPa", "MPa", "GPa",
+  "eV", "keV", "MeV", "GeV", "TeV",
+  "pV", "nV", "mV", "V", "kV",
+  "kWh",
+  "W", "nW", "mW", "kW", "MW", "GW",
+  "J", "kJ", "mJ",
+  "F", "fF", "pF", "nF", "mF",
+  "H", "fH", "pH", "nH", "mH", 
+  "C", "nC", "mC",
+  "T", "mT",
+  "K",
+  "dB"
+}
+
+function siparse(us)
+  --- fractions
+  us= string.gsub(us, "%//(%a+)", "\\per %1 ")
+  us= string.gsub(us, "%/(%a+)", "\\per %1 ")
+  us= string.gsub(us, "%*(%a+)%-1", "\\per %1 ")
+  us= string.gsub(us, "%s(%a+)%-1", "\\per %1 ")
+  --- multiples
+  us= string.gsub(us, "%*(%a+)", "\\cdot %1")
+  -- special unit - some have to be translated before standard units
+  us= string.gsub(us, "%sWb", "\\weber")
+  --- combined units (short forms)
+  for _, v in pairs(combinedunits) do
+    us= string.gsub(us, "%s"..v, "\\"..v.." ")
+  end
+  --- prefixes
+  us= string.gsub(us, "^%s?q", "\\qecto ")
+  us= string.gsub(us, "^%s?r", "\\ronto ")
+  us= string.gsub(us, "^%s?y", "\\yocto ")
+  us= string.gsub(us, "^%s?z", "\\zepto ")
+  us= string.gsub(us, "^%s?a", "\\atto ")
+  us= string.gsub(us, "^%s?f", "\\femto ")
+  us= string.gsub(us, "^%s?p", "\\pico ")
+  us= string.gsub(us, "^%s?n", "\\nano ")
+  us= string.gsub(us, "^%s?\\mu", "\\micro ")
+  us= string.gsub(us, "^%s?m", "\\milli ")
+  us= string.gsub(us, "^%s?c", "\\centi ")
+  us= string.gsub(us, "^%s?d", "\\deci ")
+  us= string.gsub(us, "^%s?da", "\\deca ")
+  us= string.gsub(us, "^%s?h", "\\hecto ")
+  us= string.gsub(us, "^%s?k", "\\kilo ")
+  us= string.gsub(us, "^%s?M", "\\mega ")
+  us= string.gsub(us, "^%s?G", "\\giga ")
+  us= string.gsub(us, "^%s?T", "\\tera ")
+  us= string.gsub(us, "^%s?P", "\\peta ")
+  us= string.gsub(us, "^%s?E", "\\exa ")
+  us= string.gsub(us, "^%s?Z", "\\zetta ")
+  us= string.gsub(us, "^%s?Y", "\\yotta ")
+  us= string.gsub(us, "^%s?R", "\\ronna ")
+  us= string.gsub(us, "^%s?Q", "\\quetta ")
+  --- binary prefixes
+  us= string.gsub(us, "^%s?Ki", "\\kibi ")
+  us= string.gsub(us, "^%s?Mi", "\\mebi ")
+  us= string.gsub(us, "^%s?Gi", "\\gibi ")
+  us= string.gsub(us, "^%s?Ti", "\\tebi ")
+  us= string.gsub(us, "^%s?Pi", "\\pebi ")
+  us= string.gsub(us, "^%s?Ei", "\\exbi ")
+  us= string.gsub(us, "^%s?Zi", "\\zebi ")
+  us= string.gsub(us, "^%s?Yi", "\\yobi ")
+  --- special units and units without short form
+  us= string.gsub(us, "%sm", "\\meter ") -- meter has to be treated seperately not to get mixed up by milli
+  us= string.gsub(us, "%sBq", "\\bequerel ")
+  us= string.gsub(us, "%s°C", "\\degreeCelsius ")
+  us= string.gsub(us, "%sGy", "\\gray ")
+  us= string.gsub(us, "%slm", "\\lumen ")
+  us= string.gsub(us, "%skat", "\\katal ")
+  us= string.gsub(us, "%slx", "\\lux ")
+  us= string.gsub(us, "%srad", "\\radian ")
+  us= string.gsub(us, "%sSv", "\\sievert ")
+  us= string.gsub(us, "%sS", "\\siemens ")
+  us= string.gsub(us, "%ssr", "\\sterradian ")
+  --- special units - non SI
+  us= string.gsub(us, "%sau", "\\astronomicalunit ")
+  us= string.gsub(us, "%sB", "\\bel ")
+  us= string.gsub(us, "%sDa", "\\dalton ")
+  us= string.gsub(us, "%s°", "\\degree ")
+  us= string.gsub(us, "%sha", "\\hectare ")
+  us= string.gsub(us, "%sh", "\\hour ")
+  us= string.gsub(us, "%s'", "\\arcminute ")
+  us= string.gsub(us, "%s''", "\\arcsecond ")
+  us= string.gsub(us, "%s%min", "\\minute ")
+  us= string.gsub(us, "%s%Np", "\\neper ")
+  us= string.gsub(us, "%s%t", "\\tonne ")
+  --- powers
+  us= string.gsub(us, "%(%a+)%^2", "%1\\squared ")
+  us= string.gsub(us, "%(%a+)%^3", "%1\\cubed ")
+  --- powers
+  us= string.gsub(us, "%s2", "\\squared ")
+  us= string.gsub(us, "%s3", "\\cubed ")
+  return us
+end
+
+function handlePu(s)
+  local inner = s:sub(5,-2) -- strip off \pu{ and }
+  --- split at space:
+  local p = string.find(inner, "%s")
+  
+  if not p then
+    local result = "\\unit{" .. siparse(" "..inner) .. "}" -- adding a space in front to make unit replacements work
+    return result
+    --- todo: könnte auch eine reine Zahl oder reine Einheit sein! Prüfen...
+--    io.stderr:write("Could not parse mhchem pu  formula " .. inner .. "\n")
+--    return "\\text{Could not parse}"
+  end
+  -- Die zweite Hälfte ist die Einheit, muss geparst werden!
+  local result = "\\qty{" .. string.sub(inner,0,p) .. "}{" .. siparse(string.sub(inner,p,-1)) .. "}"
+  ---if not result then
+  --  io.stderr:write("Could not parse mhchem formula " .. inner .. "\n")
+  --  return "\\text{Could not parse}"
+  --end
+  return result
+end
+
+function RawInline(el)
+  if (el.format == "latex" or el.format == "tex") and
+    el.text:match("\\pu{") then
+    local result = handlePu(el.text)
+    if result then
+      return pandoc.Math("InlineMath", handlePu(el.text))
+    end
+  end
+end
+
+function RawBlock(el)
+    local il = RawInline(el)
+    if il then
+      return pandoc.Para(il)
+     end
+  end
+  
+function Math(el)
+    el.text = pandoc.Math("InlineMath", string.gsub(el.text, "(\\pu%b{})", handlePu))
+   return el.text
+end
---
+++ test.md
@@ -0,0 +1,46 @@
+# Test
+
+Testtext mit Pu inline $\pu{50 Nm}$ und einmal ohne Dollars \pu{50 Nm}
+
+Physical Units (pu)
+(MathJax or KaTeX only, not for LaTeX)
+
+$\pu{123 kJ}$ $\pu{123 mm2}$
+
+There are two conventions regarding the multiplication within units.
+
+$\pu{123 J s}$
+
+$\pu{123 J*s}$
+
+There are four conventions regarding divisions.
+
+$\pu{123 kJ/mol}$
+
+$\pu{123 kJ//mol}$
+
+$\pu{123 kJ mol-1}$
+
+$\pu{123 kJ*mol-1}$
+
+There are four main conventions for writing numbers in scientific notation.
+
+$\pu{1.2e3 kJ}$
+
+$\pu{1,2e3 kJ}$
+
+$\pu{1.2E3 kJ}$
+
+$\pu{1,2E3 kJ}$
+
+If you need more control than is offered here, take a look at the siunitx extension.
+
+$\pu{13.3 \mu m}$
+$\pu{13.3 mm}$
+$\pu{.1 mmol}$
+$\pu{2 kA}$
+$\pu{3600 kWh}$
+$\pu{3600 keV}$
+$\pu{25 Bq}$
+
+it is possible to only type a unit: $\pu{kNm}$. However the filter currently cannot parse it.
